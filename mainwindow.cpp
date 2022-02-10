6862d67719b4c05428667c55eaba008684215b3e
AnserrrR <shiparev2002@gmail.com>
2022-02-10 17:19:40 +0300

    improved dark theme

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -421,6 +421,7 @@ void MainWindow::on_lightTheme_triggered()
     // Для возврата к светлой палитре достаточно
     // будет установить стандартную палитру из темы оформления
     qApp->setPalette(style()->standardPalette());
+    qApp->setStyleSheet("");
 }
 
 
@@ -432,19 +433,26 @@ void MainWindow::on_darkTheme_triggered()
     // Настраиваем палитру для цветовых ролей элементов интерфейса
     darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
     darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::Base, QColor(35, 35, 35));
     darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipBase, QColor(25, 25, 25));
     darkPalette.setColor(QPalette::ToolTipText, Qt::white);
     darkPalette.setColor(QPalette::Text, Qt::white);
     darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
     darkPalette.setColor(QPalette::ButtonText, Qt::white);
     darkPalette.setColor(QPalette::BrightText, Qt::red);
     darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    darkPalette.setColor(QPalette::Highlight, QColor(0, 160, 255));
+    darkPalette.setColor(QPalette::HighlightedText, QColor(35, 35, 35));
+    darkPalette.setColor(QPalette::Active, QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::WindowText, Qt::darkGray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Light, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::PlaceholderText, QColor(100, 100, 100));
 
     // Устанавливаем данную палитру
     qApp->setPalette(darkPalette);
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 }
 
